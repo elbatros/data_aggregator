4f9118667b6a7b2590814091b978c803d35d3acb
Hashem Raslan <hashem.raslan@gmail.com>
2017-11-19 16:11:23 -0500

    Cleanup and refactoring

--- dataaggregator.py
+++ dataaggregator.py
@@ -1,27 +1,20 @@
 import pandas as pd
 import csv
 import sys
-import fileinput
 
-
-VALUE_KEY = 'value' #TODO remove \r
-rollup_target = None#['y','m','d']
 
 #TODO: Can the value key not be "value"
-
-class DataRoller:
-	def __init__(self, input_file, rollup_target, value_key = VALUE_KEY, \
-			aggregation_type = 'sum', delimiter = '\t', lineterminator = '\n'):
+class DataAggregator:
+	def __init__(self, input_file, rollup_target, value_key = 'value', \
+				 delimiter = '\t', lineterminator = '\n'):
 		
 		self.__input_file = input_file
 		self.__rollup_target = rollup_target 		
-
 		self.__value_key = value_key 				
 		self.__input_file_delimiter = delimiter
 		self.__input_file_lineterminator = lineterminator
 		self.__data = None
 		self.__result = []
-		self.__aggregation_type = aggregation_type 
 
 	def __load_file(self):
 		try:
@@ -53,18 +46,19 @@ class DataRoller:
 			self.__data = self.__data.drop(col, 1)
 
 
-	def __create_aggregated_groups(self):
+	def __create_aggregated_groups(self, aggregation_type):
+		results = []
 		#Create groups to iterate over
 		for idx in range(len(self.__rollup_target) + 1):
 			target_grp_cols = self.__rollup_target[:len(self.__rollup_target)-idx]
 
 			rol = {}
 			if len(target_grp_cols) == 0:
-				rol[self.__value_key] = self.__data.agg(self.__aggregation_type)[self.__value_key]
-				self.__result.append(rol)
+				rol[self.__value_key] = self.__data.agg(aggregation_type)[self.__value_key]
+				results.append(rol)
 			else:
 				# Group by target_grp_cols and calculate sum
-				grouped_df = self.__data.groupby(target_grp_cols).agg(self.__aggregation_type)
+				grouped_df = self.__data.groupby(target_grp_cols).agg(aggregation_type)
 				for grp_idx, row in grouped_df.iterrows():
 					if len(target_grp_cols) == 1: #Special case when only grouped by a single column. grp_idx is not a tuple
 						rol = {target_grp_cols[0]: grp_idx} 
@@ -72,21 +66,21 @@ class DataRoller:
 						rol = {target_grp_cols[i]: grp_idx[i] for i in range(len(target_grp_cols))}
 
 					rol[self.__value_key] = row[self.__value_key]
-					self.__result.append(rol)
+					results.append(rol)
+		return results
 
-	def run(self):
+	def rollup(self):
 		self.__load_file()
 
 		#If no rollup target identified, use all columns
 		if self.__rollup_target == []:
 			self.__rollup_target = list(self.__data.columns)
 			self.__rollup_target.remove(self.__value_key)
-			#TODO: Do we need to worry about order?
 
 		self.__cleanup()
-		self.__create_aggregated_groups()
+		self.__result = self.__create_aggregated_groups('sum')
 
-	def save_to_file(self, file_name):
+	def save_result(self, file_name):
 		with open(file_name, 'wb') as output_file:
 			keys = self.__rollup_target
 			keys.append(self.__value_key)
@@ -97,9 +91,9 @@ class DataRoller:
 
 
 def main(input_file,  rollup_target):
-	data_roller = DataRoller(input_file, rollup_target)
-	data_roller.run()
-	data_roller.save_to_file('out.txt')
+	data_roller = DataAggregator(input_file, rollup_target)
+	data_roller.rollup()
+	data_roller.save_result('out.txt')
 
 if __name__ == '__main__':
 	input_file = sys.stdin
